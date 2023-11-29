e9b3eedbee1254e551722d1fa198b9ecb70fcf97
Caio Salvador Rohwedder <caiosalvador96@gmail.com>
2023-11-29 14:11:56 -0700

    Create packing candidates based on data reuse

--- mlir/lib/Dialect/Affine/Transforms/LoopPacking.cpp
+++ mlir/lib/Dialect/Affine/Transforms/LoopPacking.cpp
@@ -102,6 +102,101 @@ static void getMemRefsInForOp(AffineForOp forOp, SetVector<Value> &memRefs,
   });
 }
 
+/// Check if a memRef is loaded in a forOp.
+static bool isMemRefLoadedInForOp(AffineForOp forOp, Value memRef) {
+  auto walkRes = forOp.walk([&](Operation *op) {
+    if (auto loadOp = dyn_cast<AffineLoadOp>(op)) {
+      if (memRef == loadOp.getMemRef())
+        return WalkResult::interrupt();
+    }
+    return WalkResult::advance();
+  });
+
+  return walkRes.wasInterrupted();
+}
+
+/// Returns true if all accesses to memRef inside forOp
+/// are invariant to forOp.
+static bool memRefAccessesInvariantToLoop(AffineForOp forOp, Value memRef) {
+
+  SmallVector<AffineLoadOp> loads;
+  SmallVector<AffineStoreOp> stores;
+
+  // Gather loads and stores.
+  forOp.walk([&](Operation *op) {
+    if (auto loadOp = dyn_cast<AffineLoadOp>(op)) {
+      if (memRef == loadOp.getMemRef())
+        loads.push_back(loadOp);
+    } else if (auto storeOp = dyn_cast<AffineStoreOp>(op)) {
+      if (memRef == storeOp.getMemRef())
+        stores.push_back(storeOp);
+    }
+  });
+
+  if (loads.empty() && stores.empty())
+    return true;
+
+  // Collects indices used in all loads and stores.
+  DenseSet<Value> accessIndices;
+  for (AffineLoadOp load : loads)
+    for (Value operand : load.getIndices())
+      accessIndices.insert(operand);
+  for (AffineStoreOp store : stores)
+    for (Value operand : store.getIndices())
+      accessIndices.insert(operand);
+
+  // Collect the IV thisLoopIV of forOp and add dependent loop IVs:
+  // If thisLoopIV is used in any other inner loops as an upper or lower bound
+  // operand, add the IV of these inner loops as a dependent loop IVs.
+  // Do this iteratively for the new IVs as well.
+  DenseSet<Value> loopIVs;
+  DenseSet<Value> checkDepsList;
+  auto thisLoopIV = forOp.getInductionVar();
+  checkDepsList.insert(thisLoopIV);
+
+  // Check dependencies and then move loop IV to loopIVs.
+  while (!checkDepsList.empty()) {
+    // Pop IV from deps list
+    Value currLoopIV = *checkDepsList.begin();
+    checkDepsList.erase(currLoopIV);
+
+    // Check if it is was not visited already
+    if (!loopIVs.insert(currLoopIV).second)
+      continue;
+
+    AffineForOp currForOp = getForInductionVarOwner(currLoopIV);
+
+    currForOp.walk([&](AffineForOp walkForOp) {
+      if (walkForOp == currForOp)
+        return WalkResult::advance();
+
+      for (auto upOperand : walkForOp.getUpperBoundOperands()) {
+        if (currLoopIV == upOperand) {
+          checkDepsList.insert(walkForOp.getInductionVar());
+          return WalkResult::advance();
+        }
+      }
+      for (auto lbOperand : walkForOp.getLowerBoundOperands()) {
+        if (currLoopIV == lbOperand) {
+          checkDepsList.insert(walkForOp.getInductionVar());
+          return WalkResult::advance();
+        }
+      }
+      return WalkResult::advance();
+    });
+  }
+
+  // Check if all indices are invariant to all IVs
+  for (auto accessVal : accessIndices) {
+    for (auto loopVal : loopIVs)
+      if (!isAccessIndexInvariant(loopVal, accessVal))
+        return false;
+  }
+
+  // If all acesses are invariant, the loop is invariant to the memref.
+  return true;
+}
+
 /// Stores information of an AffineForOp.
 class PackingLoopInfo {
 public:
@@ -119,6 +214,20 @@ public:
     this->depth = getNestingDepth(this->forOp) + 1;
     this->tripCount = getApproxTripCount(this->forOp);
   };
+};
+
+/// Information about a packing candidate.
+class PackingAttributes {
+public:
+  /// ID, for printing purposes and for enabling manual selection of candidates.
+  int64_t id = -1;
+  /// Target memRef
+  Value memRef;
+  /// LoopInfo of target forOp.
+  PackingLoopInfo *loop;
+
+  PackingAttributes(Value memRef, PackingLoopInfo &loop)
+      : memRef(memRef), loop(&loop) {}
 };
 
 /// Analyse and apply packing to a loop and its nestings.
@@ -154,6 +263,29 @@ void LoopPacking::runOnOuterForOp(AffineForOp outerForOp,
   if (maxLoopDepth < requiredLoopDepth) {
     LLVM_DEBUG(
         dbgs() << "[DEBUG] Not packing, loops have a depth smaller than 3.\n");
+    return;
+  }
+
+  // Structure storing packing candidates.
+  SmallVector<PackingAttributes> packingCandidates;
+
+  // Reuse filter
+  // Possible packing candidates: all loop and memref combinations.
+  // Only add candidates that show reuse, that is, all accesses to the memRef
+  // are invariant to the loop.
+  for (auto &loopInfo : loopInfoMap) {
+    for (const auto &memRef : memRefs) {
+      AffineForOp forOp = loopInfo.first;
+      PackingLoopInfo &forOpInfo = loopInfo.second;
+      // Skip outermost loop (depth > 1): would pack the entire memRef.
+      if (forOpInfo.depth > 1 &&
+          isMemRefLoadedInForOp(forOp, memRef) &&
+          memRefAccessesInvariantToLoop(forOp, memRef))
+        packingCandidates.push_back(PackingAttributes(memRef, forOpInfo));
+    }
+  }
+  if (packingCandidates.empty()) {
+    LLVM_DEBUG(dbgs() << "[DEBUG] Not packing, no invariant loops found.\n");
     return;
   }
 }
